252dd1961d1d07104560413940f41bb949f06054
Parschhurra Zimero <119706024+pzimero@users.noreply.github.com>
2023-01-08 22:41:34 -0500

    toggle button and cleaning up code

--- shared.js
+++ shared.js
@@ -2,24 +2,42 @@ var backdrop = document.querySelector('.backdrop')
 var modal = document.querySelector('.modal')
 var modalNoButton = document.querySelector('.modal__action--negative')
 var selectPlanButtons = document.querySelectorAll('.plan button')
+var toggleButton = document.querySelector('.toggle-button')
+var mobileNav = document.querySelector('.mobile-nav')
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block'
-        backdrop.style.display = 'block'
+        // modal.style.display = 'block'
+        // backdrop.style.display = 'block'
+        // modal.className = 'open' will overwrite the complete list
+        modal.classList.add('open')
+        backdrop.classList.add('open')
     })
 }
- backdrop.addEventListener('click', function() {
-    backdrop.style.display = 'none'
-    modal.style.display = 'none'
- })
+backdrop.addEventListener('click', function() {
+    // mobileNav.style.display = 'none'
+    mobileNav.classList.remove('open')
+    closeModal()
+})
 
- modalNoButton.addEventListener('click', function() {
-    backdrop.style.display = 'none'
-    modal.style.display = 'none'
- })
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal)
+}
 
- function closeModal() {
-    backdrop.style.display = 'none'
-    modal.style.display = 'none'
- }
+
+function closeModal() {
+    // backdrop.style.display = 'none'
+    // modal.style.display = 'none'
+    if (modal) {
+        modal.classList.remove('open')
+    }
+    backdrop.classList.remove('open')
+}
+
+toggleButton.addEventListener('click', function() {
+    // mobileNav.style.display = 'block'
+    // backdrop.style.display = 'block'
+    mobileNav.classList.add('open')
+    backdrop.classList.add('open')
+
+ })
